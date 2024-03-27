0020c529402815a81e7deed8c362e3addbb2562f
Layla El Adouli <r0997884@student.thomasmore.be>
2024-03-27 14:18:23 +0100

    change 1 .

--- src/main/java/be/thomasmore/web/controllers/HomeController.java
+++ src/main/java/be/thomasmore/web/controllers/HomeController.java
@@ -13,26 +13,12 @@ import static java.time.LocalDateTime.now;
 @Controller
 public class HomeController {
 
-    @GetMapping({"/", "/home"})
-    public String home(Model model) {
+    @GetMapping({"/" })
+    public String home() {
         return "home";
     }
 
-    @GetMapping("/about")
-    public String about(Model model) {
-        model.addAttribute("myName", "Anne Van Goethem");
-        model.addAttribute("myStreet", "Koning Albertlaan 23");
-        model.addAttribute("myCity", "Leuven");
-        return "about";
-    }
 
-    @GetMapping("/pay")
-    public String pay(Model model) {
-        LocalDateTime now = now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        model.addAttribute("today", formatter.format(now));
-        model.addAttribute("payday", formatter.format(now.plusDays(30)));
-        model.addAttribute("isWeekend", now.getDayOfWeek() == DayOfWeek.SATURDAY || now.getDayOfWeek() == DayOfWeek.SUNDAY);
-        return "pay";
-    }
+
+
 }
