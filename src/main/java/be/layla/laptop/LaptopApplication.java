be34f2344c6d96c50e98a541f433eb0675ac7b25
Layla El Adouli <r0997884@student.thomasmore.be>
2024-05-30 15:09:32 +0200

    creating the first version of home.

--- src/main/java/be/layla/laptop/LaptopApplication.java
+++ src/main/java/be/layla/laptop/LaptopApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebApplication {
+public class LaptopApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(WebApplication.class, args);
+        SpringApplication.run(LaptopApplication.class, args);
     }
 
 }
--- src/main/java/be/layla/laptop/controllers/HomeController.java
+++ src/main/java/be/layla/laptop/controllers/HomeController.java
@@ -4,21 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.DayOfWeek;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import static java.time.LocalDateTime.now;
-
 @Controller
 public class HomeController {
 
-    @GetMapping({"/" })
-    public String home() {
+
+    @GetMapping({"/","/home" })
+    public String home(Model model) {
         return "home";
     }
-
-
-
-
 }
