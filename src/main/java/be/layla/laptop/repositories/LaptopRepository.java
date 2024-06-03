3ef57e8ebc53fbeddcc6fe03e1a4b8fb9b6e05d7
Layla El Adouli <r0997884@student.thomasmore.be>
2024-06-03 17:40:08 +0200

    correcting the button of all categories .

--- src/main/java/be/layla/laptop/repositories/LaptopRepository.java
+++ src/main/java/be/layla/laptop/repositories/LaptopRepository.java
@@ -14,9 +14,9 @@ public interface LaptopRepository extends CrudRepository<Laptop, Integer> {
 
     @Query("SELECT l FROM Laptop l WHERE " +
             "(:brandName IS NULL OR :brandName = 'all' OR l.brandName = :brandName) AND" +
-            "(:priceRange IS NULL OR " +
-            "(CASE WHEN :priceRange = 'less' THEN l.price < 50 ELSE l.price >= 50 END)) AND " +
-            "(:ramRange IS NULL OR " +
+            "(:priceRange IS NULL OR :priceRange = 'all' OR " +
+            "(CASE WHEN :priceRange = 'less' THEN l.price < 400 ELSE l.price >= 400 END)) AND " +
+            "(:ramRange IS NULL OR :ramRange = 'all' OR " +
             "(CASE WHEN :ramRange = 'less' THEN l.ram < 16 ELSE l.ram >= 16 END))")
     List<Laptop> findByFilter(@Param("brandName") String brandName,
                               @Param("priceRange") String priceRange,
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -27,13 +27,13 @@ VALUES (1, 'acer Aspire 5 15 Slim Laptop', 'acer', 'Laptop', 'Gray', 15.6, 128, 
         'Equipped with Intel Core i3-1215U processor, 1.2GHz(Up to Turbo Boost 4.4 GHz, 6 cores, 8 threads), this processor ensures reliable performance for everyday computing needs, from work tasks to multimedia entertainment.<br>'
             || 'Up to 2TB SSD storage capacity is suitable for saving all your files and provides enough space to save more data.<br>'
             || 'Stay connected with built-in WiFi and Bluetooth capabilities. The laptop features an integrated HP TrueVision 720p HD camera with a privacy shutter and integrated Dual Array Digital Microphones.'),
-        (6, 'BaseBook 16GB RAM Windows 11 Laptop', 'BHWW', 'BaseBook-Spare-Silver', 'Gray', 15.6, 512, 'SSD', 16, 'Windows 11',
-        'HD Audio, Privacy Cam, Numeric Keypad', 'Integrated', 624.20, 'https://m.media-amazon.com/images/I/61seZIVv7TL._AC_SY450_.jpg', 'https://www.amazon.com/stores/page/7089B099-6CB7-400B-8D52-D68B49D95E82?ingress=2&visitId=adac6c2e-e125-4db7-a927-d93daea16429&ref_=ast_bln',
+        (6, 'BaseBook 16GB RAM Windows 11 Laptop', 'BHWW', 'BaseBook-Spare-Silver', 'Gray', 15.6, 512, 'SSD', 12, 'Windows 11',
+        'HD Audio, Privacy Cam, Numeric Keypad', 'Integrated', 445.50, 'https://m.media-amazon.com/images/I/61seZIVv7TL._AC_SY450_.jpg', 'https://www.amazon.com/stores/page/7089B099-6CB7-400B-8D52-D68B49D95E82?ingress=2&visitId=adac6c2e-e125-4db7-a927-d93daea16429&ref_=ast_bln',
         'Windows 11 - Beautiful, more consistent new design, Great window layout options, Better multi-monitor functionality, Improved performance features, New videogame selection and capabilities, Compatible with Android Apps.<br>'
             || 'The laptop is a budget-friendly option for kids, students, and businesses, ideal for academic tasks, work assignments, and entertainment.<br>'
             || 'We provide 7X24 24-hour pre-sales and 12-month local after-sales maintenance services'),
         (7, 'Laptop Computer for Gaming and Work', 'BHWW', 'BaseBook Pro-Silver', 'Silver', 16.1, 1000, 'Core i5', 32, 'Windows 11 Pro',
-        'Fingerprint Reader', 'Integrated', 445.51, 'https://m.media-amazon.com/images/I/71biF12xEQL._AC_SX679_.jpg', 'https://www.amazon.com/stores/page/7089B099-6CB7-400B-8D52-D68B49D95E82?ingress=2&visitId=adac6c2e-e125-4db7-a927-d93daea16429&ref_=ast_bln',
+        'Fingerprint Reader', 'Integrated', 624.20, 'https://m.media-amazon.com/images/I/71biF12xEQL._AC_SX679_.jpg', 'https://www.amazon.com/stores/page/7089B099-6CB7-400B-8D52-D68B49D95E82?ingress=2&visitId=adac6c2e-e125-4db7-a927-d93daea16429&ref_=ast_bln',
         'It is equipped with a highly efficient Intel i5 processor (The main frequency is up to 1.1 GHz). The powerful processor makes it run faster and is more powerful than other processors, and the upgraded CPU also brings substantial improvements in overall system performance, such as multitasking capabilities, graphics processing, video editing, gaming.<br>'
             || 'The laptop computer is designed in ratio of 16:10 (usually in ratio of 16:9), so the larger vertical space reduces mouse sliding and enhances productivity and creativity in coding, document editing, graphic designing and video editing.'),
         (8, 'ACEMAGIC 16.1“ Gaming Laptop ', 'ACEMAGIC', 'AX16PRO', 'Gray', 16.1, 512, 'Ryzen 7 5700U', 16, 'Windows 11',
@@ -66,49 +66,50 @@ VALUES (1, 'acer Aspire 5 15 Slim Laptop', 'acer', 'Laptop', 'Gray', 15.6, 128, 
 INSERT INTO ACCESSORIE (
      ID, TITLE, BRAND_NAME, PRODUCT_NAME, COLOR, FEATURE, PRICE, IMG_URL, DESCRIPTION
 )
-VALUES (1, 'White Rose Bag', 'canvaslife', 'Laptop Case', 'White rose', 'Waterproof Pattern', 13, 'https://m.media-amazon.com/images/I/91VrApHSa0L._AC_SX679_.jpg',
+VALUES (1, 'White Rose Bag', 'canvaslife', 'Laptop Case', 'White rose', 'Waterproof Pattern', 13.60, 'https://m.media-amazon.com/images/I/91VrApHSa0L._AC_SX679_.jpg',
         'Canvaslife Waterproof shockproof laptop shoulder bag is light and easy to carry.<br>'
             || 'Say Goodbye to dull color laptop bag,our special patten laptop bag make you unique.<br>'
             || 'Made of high quality canvas and thick metal zipper puller,metal buckle.Our bag is strong and durable to use.'),
-       (2, 'TECKNET Wireless Mouse', 'TECKNET', 'USB', 'Red', '24 Months Battery Life', 16.50, 'https://m.media-amazon.com/images/I/71ykVZTPglL._AC_SY450_.jpg',
+       (2, 'TECKNET Wireless Mouse', 'TECKNET', 'Mouse', 'Red', '24 Months Battery Life', 16.50, 'https://m.media-amazon.com/images/I/71ykVZTPglL._AC_SY450_.jpg',
         'Connect two devices simultaneously using either Bluetooth(3.0/5.0) or USB A connection for a wireless mouse experience. Seamlessly switch between the connected devices with a simple slide of the switch button.<br>'
             || 'Enjoy a noiseless experience with this silent mouse. The wireless bluetooth mouse is designed to provide quiet operation, making it suitable for environments where silence is appreciated, such as libraries or offices.<br>'
             || 'The wireless silent mouse is compatible with various systems including Windows/macOS/Chrome/Linux.'),
-       (3, 'USB C Laptop Charger 65w', 'Lamicall', 'Laptop Charge', 'Black', 'Fast Charging', 14.99, 'https://m.media-amazon.com/images/I/61vaObX0XtL._AC_SX450_.jpg',
-        'Output Max: 65w ~ 20v 3.25a Compatible 45W~ 20v 2.25a/and Less Than 65W Input Volt Range: 110-240v Wattage: 65w Max;Plug Type: USB C Type C Port; Condition: 100% New.<br>'
-            || 'This 65w 45w USB-C Charger Universal Replacement For Lenovo HP Acer Sumsang Asus Chromebook Series,Dell Xps 13 Series And More....<br>'),
-       (4, 'Laptop Stand Riser', 'OMOTON', 'Laptops stand', 'Silver', 'Allows extra air circulation', 28.49, 'https://m.media-amazon.com/images/I/51jVIxFX24L._AC_SY450_.jpg',
+       (3, 'Compact Mobile Document Scanner', 'Brother', 'Document scanner', 'White', 'easy to using', 120.50, 'https://f.media-amazon.com/images/I/61hKEGMQBCL._AC_SX679_.jpg',
+        'Scans color and black and white documents a blazing speed up to 16ppm.<br>'
+            || 'The DS-640 mobile scanner is powered via an included micro USB 3. 0 cable allowing you to use it even where there is no outlet available. Plug it into you PC or laptop and you are ready to scan.<br>'
+            || 'Use the Brother free iPrint&Scan desktop app for scanning to multiple “Scan-to” destinations like PC, Network, cloud services, Email and OCR.'),
+       (4, 'Laptop Stand Riser', 'OMOTON', 'Laptop stand', 'Silver', 'Allows extra air circulation', 28.49, 'https://m.media-amazon.com/images/I/51jVIxFX24L._AC_SY450_.jpg',
         'Laptop stand for elevating a laptop to a comfortable, productive height; ideal for home, office, or on the go.<br>'
             || 'Includes a laptop stand made of durable, lightweight aluminum with nonslip rubber surface pads to keep laptop in place.<br>'
             || 'Adjustable Z-style shape allows for easily raising and lowering or changing the degree of tilt for optimal ergonomics.'),
-       (5, 'Taygeer Travel Backpack for Women', 'Tygeer', 'Laptop and travel Backpack', 'pink', 'Wet bag is designed to isolate wet items. Valid for Flights, Business Hiking,...', 23.99, 'https://m.media-amazon.com/images/I/711bhLQOb9L._AC_SX425_.jpg',
+       (5, 'Taygeer Travel Backpack for Women', 'Tygeer', 'Backpack', 'pink', 'Wet bag is designed to isolate wet items. Valid for Flights, Business Hiking,...', 23.99, 'https://m.media-amazon.com/images/I/711bhLQOb9L._AC_SX425_.jpg',
         'Large carry on backpack for women with built in USB charger outside and built in charging cable inside, this suitcase backpack for travel offers you a more convenient way to charge your phone while walking.<br>'
             || 'Dimensions 17*11*7inch, Light Weight 1.2Kg, has 3 main large compartments and 9 allocated inner pockets and side pockets. Makes your item organized and easier to find. At the samen is slightly waterproof.<br>'
             || 'Flight Approved Laptop Backpack: 180 degree zipper closure helps you pass through the security check quickly. Travel essentials for women fits overhead and under the seat.'),
-       (6, 'Bluetooth Headphones, Neckband Bluetooth Headphones', 'YOSDOM', 'Headphones', 'Silver', 'Noise cancelling microphones', 29.99, 'https://m.media-amazon.com/images/I/61lQuD+9VZL._AC_SY450_.jpg',
+       (6, 'Bluetooth Headphones, Neckband Bluetooth Headphones', 'YOSDOM', 'Headphones', 'Silver', 'Noise cancelling microphones', 28.99, 'https://m.media-amazon.com/images/I/61lQuD+9VZL._AC_SY450_.jpg',
         '24 hours of consecutive playback. You can use these neckband Bluetooth headphones from dawn to dusk.<br>'
             || 'Easy to control phone calls. Built-in noise cancelling microphones, these neckband headphones allow you to enjoy a crystal-clear phone call.<br>'
             || 'Unique magnetic SHARK-LIKE design. Both magnetic earbuds headphones can be attached to the neckband when not in use.'),
-       (7, 'Large Laptop Case', 'canvaslife', 'Laptop Case', 'Pink', 'Made of durable water resistant polyester fabric.', 40, 'https://m.media-amazon.com/images/I/71f9jCubIGL._AC_SX679_.jpg',
+       (7, 'Large Laptop Case', 'Lenovo', 'Laptop Case', 'Pink', 'Made of durable water resistant polyester fabric.', 40.50, 'https://m.media-amazon.com/images/I/71f9jCubIGL._AC_SX679_.jpg',
         'PERFECT CAPACITY & MULTIPLE COMPARTMENTS- 15.5"L x 3.5"W x 11.5"H; weight: 1.5 lbs, perfect to throw under seat on an airplane. Three compartments to meet all your needs well.<br>'
             || 'The dedicated laptop compartment is fully padded. Laptop case can offer the optimal protection for your laptop. You don''t need to worry about your notebook getting crashed on the travelling.'),
-       (8, 'Chip Card Reader Scanner Case', ' plustek', 'Card reader', 'White', 'Contactless and Chip Reader with USB Charge Cable', 13, 'https://m.media-amazon.com/images/I/71yvy8FpmlL._AC_SY450_.jpg',
-        'Square Contactless and Chip Reader, Square Reader for magstripe (with headset jack) and USB Cable.<br>'
-            || 'Premium hard EVA material covered by quality PU material, is not only semi-waterproof, shockproof, but also with a comfortable soft touch.<br>'
-            || 'This chip card reader case is lightweight and compact to fit in your pocket, carry-on or luggage for improved traveling protection.'),
-       (9, 'Desktop Document Scanner', ' plustek', 'Document scanner', 'Black', '30 Pages Per Minute.', 250, 'https://m.media-amazon.com/images/I/81j4HdV+7KL._AC_SX679_.jpg',
+       (8, 'Bluetooth Headphones Wireless', 'YOSDOM', 'Headphones', 'Black', 'long lasting power time', 40, 'https://f.media-amazon.com/images/I/714GIl8hvML._AC_SX522_.jpg',
+        'bluetooth earbuds come with a compact charging case, providing 90 hours of total listening time for the wireless earbuds. Each ear bud lasts all day and offers up to 8 hours playback on a single charge,the small case can provide an additional 5 full charges.<br>'
+            || 'The wireless headphones bluetooth ear buds featuring 10mm double-layer diaphragm drivers deliver a powerful bass and super crisp sound and immerse yourself in superior sound quality.<br>'
+            || 'The headphones with earhooks have Bluetooth 5.3 capability for a strong and continuous connection within a 30ft range.'),
+       (9, 'Desktop Document Scanner', ' Brother', 'Document scanner', 'Black', '30 Pages Per Minute.', 250, 'https://m.media-amazon.com/images/I/81j4HdV+7KL._AC_SX679_.jpg',
         'Embedded File Search Engine - Full Text Search is a comprehensive search system.<br>'
             || 'Info Barcode function - automatic categorization of complicate documentation and data with 1D or 2D Barcode page.<br>'
             || 'Easy send scanned files to FTP server or personal NAS (FTP) with PDFs , Jpeg , TIFF or Png format.'),
-       (10, 'Double Desktop Stand Holder with Adjustable Dock.', 'OMOTON', 'Double stand', 'Silver', 'it doesn''t make my desk look messy.', 30, 'https://m.media-amazon.com/images/I/61U-1XwBrBL._AC_SY450_.jpg',
+       (10, 'Double Desktop Stand Holder with Adjustable Dock.', 'OMOTON', 'Laptop stand', 'Silver', 'it doesn''t make my desk look messy.', 30, 'https://m.media-amazon.com/images/I/61U-1XwBrBL._AC_SY450_.jpg',
         'Double dock stand capable of supporting two laptops simultaneously (e.g. MacBook, Samsung, Microsoft Surface, etc).<br>'
             || 'Adjustable width compatible with various laptops as well as the thickness of notebooks varying from 0.55in / 14mm to 1.65in / 42mm.<br>'
             || 'Its anodized aluminum is scratch-resistant and its wide base guarantees stability to prevent your laptop(s) from falling down.'),
-       (11, 'Web Camera for Computer/Laptop/MacBook.', 'GUSGU', 'Webcam', 'Black', 'Using Flash, USB Streaming Camera.', 35.50, 'https://m.media-amazon.com/images/I/71Q8WxhgxML._AC_SX679_.jpg',
+       (11, 'Web Camera for Computer/Laptop/MacBook.', 'GUSGU', 'Webcam', 'Black', 'Using Flash, USB Streaming Camera.', 50.50, 'https://m.media-amazon.com/images/I/71Q8WxhgxML._AC_SX679_.jpg',
         'The onboard digital microphone of G910 webcam helps recording your voice clearly, and reducing environmental noises on video calling and conferencing.<br>'
             || 'Webcam works with most video chatting apps under Windows and macOS like Zoom, Teams, Skype, etc, plug and play, no driver programs needed.<br>'
             || 'Webcam adopts built-in privacy cover design, which is easier to use and more reliable than common products, no additional webcam cover needed.'),
-       (12, 'Wireless Keyboard and Mouse', 'TECKNET', 'Keyboard and Mouse', 'Purple', 'Wrist Rest', 33.60, 'https://m.media-amazon.com/images/I/71CNnh4yTJL._AC_SY450_.jpg',
+       (12, 'Wireless Keyboard and Mouse', 'TECKNET', 'Mouse', 'Purple', 'Wrist Rest', 33.60, 'https://m.media-amazon.com/images/I/71CNnh4yTJL._AC_SY450_.jpg',
         'The mouse requires a single AA battery, while the keyboard requires two AA batteries.<br>'
             || 'This keyboard is easy to setup.Plug and Play, No drivers required.Compatible with Windows 2000/XP/7/8/10, Vista,Raspberry Pi 3/4, Mac OS(Note: Multimedia keys may not fully compatible with Mac, OS System).Works with your PC, laptop.<br>'
             || 'The USB receiver is plugged into the battery compartment of the mouse.The keyboard and mouse connected to the same dongle.'),
@@ -120,10 +121,10 @@ VALUES (1, 'White Rose Bag', 'canvaslife', 'Laptop Case', 'White rose', 'Waterpr
         'The award-winning A2 Plus computer speakers offer true stereo sound that you can easily connect to wirelessly from up to 100 feet away.<br>'
             || 'A pair of A2 Plus wireless PC speakers, 6.5 ft speaker wire, power supply & cord, 5 ft USB cable, 5 ft mini-jack audio cable, microfiber speaker and cable bags, setup guide.<br>'
             || 'Room filling sound in a small size. Easily connect an Audioengine S6 or S8 subwoofer via the RCA variable output. And at 6 x 4 x 5.25 inches each, the A2 Plus system sounds much bigger than it is.'),
-       (15, 'Web Camera for Computer/Laptop/MacBook.', 'GUSGU', 'Webcam', 'Black', 'Using Flash, USB Streaming Camera', 35.50, 'https://m.media-amazon.com/images/I/71Q8WxhgxML._AC_SX679_.jpg',
-        'The onboard digital microphone of G910 webcam helps recording your voice clearly, and reducing environmental noises on video calling and conferencing.<br>'
-            || 'Webcam works with most video chatting apps under Windows and macOS like Zoom, Teams, Skype, etc, plug and play, no driver programs needed.<br>'
-            || 'Webcam adopts built-in privacy cover design, which is easier to use and more reliable than common products, no additional webcam cover needed.');
+       (15, 'Pro Streaming Web Camera with Microphone', 'XPCAM', 'Webcam', 'Black', 'Webcam with privacy shutter', 35.50, 'https://f.media-amazon.com/images/I/61ZiFsDP8WL._AC_UY327_FMwebp_QL65_.jpg',
+        'Web camera with full HD glass lens delivers razor sharp video that makes you look like a pro, with automatic light correction and HDR technology, computer camera auto adjusts color and brightness for natural lighting even in dim light.<br>'
+            || 'USB webcam with 6ft USB power cable, no additional driver required, you just need to plug the USB port into your computer, then you can use video software such as facetime for calling.<br>'
+            || 'HD webcam compatible with youtube, skype, facetime, zoom, and more; you can widely use it for online teaching, video calling, recording.');
 
 
 
