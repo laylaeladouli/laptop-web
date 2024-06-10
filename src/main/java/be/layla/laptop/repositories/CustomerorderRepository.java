932cf603e131344c9a6df1c504ca20e1cff85dc9
Layla El Adouli <r0997884@student.thomasmore.be>
2024-06-10 15:37:31 +0200

    creating CustomerorderRepository class .

---
+++ src/main/java/be/layla/laptop/repositories/CustomerorderRepository.java
@@ -0,0 +1,12 @@
+package be.layla.laptop.repositories;
+
+import be.layla.laptop.model.Customerorder;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerorderRepository extends CrudRepository<Customerorder, Integer> {
+
+
+
+
+
+}
