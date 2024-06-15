ce675652f9ed5e4f2aa449acbd11238dac257fcf
Layla El Adouli <r0997884@student.thomasmore.be>
2024-06-15 12:39:50 +0200

    adding @GetMapping and @PostMapping in LaptopAdminController class.

--- src/main/java/be/layla/laptop/controllers/admin/LaptopAdminController.java
+++ src/main/java/be/layla/laptop/controllers/admin/LaptopAdminController.java
@@ -2,11 +2,13 @@ package be.layla.laptop.controllers.admin;
 
 import be.layla.laptop.model.Laptop;
 import be.layla.laptop.repositories.LaptopRepository;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -25,6 +27,7 @@ public class LaptopAdminController {
     public Laptop findLaptop(@PathVariable(required = false) Integer id) {
         logger.info("findLaptop " + id);
         if (id == null) return new Laptop();
+
         Optional<Laptop> optionalLaptop = laptopRepository.findById(id);
         if (optionalLaptop.isPresent())
             return optionalLaptop.get();
@@ -41,10 +44,39 @@ public class LaptopAdminController {
 
 
     @PostMapping("/laptopedit/{id}")
-    public String laptopEditPost(@PathVariable Integer id, Laptop laptop){
+    public String laptopEditPost(Model model,
+                                 @PathVariable Integer id,
+                                 @Valid Laptop laptop,
+                                 BindingResult bindingResult) {
 
         logger.info("laptopEditPost" + id + " -- new name=" + laptop.getBrandName());
+        if (bindingResult.hasErrors()) {
+            return "admin/laptopedit";
+        }
         laptopRepository.save(laptop);
-        return "redirect:/laptopedit/" + id;
+        return "redirect:/admin/laptopedit/" + id;
     }
+
+
+    @GetMapping("/laptopnew")
+    public String laptopNew(Model model) {
+        model.addAttribute("laptops", laptopRepository.findAll());
+        logger.info("laptopnew ");
+        return "admin/laptopnew";
+    }
+
+
+    @PostMapping("/laptopnew")
+    public String laptopNewPost(Laptop laptop) {
+        logger.info("laptopNewPost -- new name=" + laptop.getBrandName());
+        laptopRepository.save(laptop);
+        return "redirect:/admin/laptopdetails";
+    }
+
+
+
+
+
+
+
 }
